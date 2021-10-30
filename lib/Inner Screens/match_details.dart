618d0a37a73deb8c3ba9f473a55056c61732cf26
AlexandrosChristou11 <alexandros_christou@hotmail.com>
2021-10-30 12:12:47 +0300

    - Bind Matches Provider with match_details  Inner Screen

--- lib/Inner Screens/match_details.dart
+++ lib/Inner Screens/match_details.dart
@@ -10,6 +10,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sep21/Provider/DarkTheme.dart';
+import 'package:sep21/Provider/Matches.dart';
 import 'package:sep21/Screens/cart.dart';
 import 'package:sep21/Screens/feed.dart';
 import 'package:sep21/Screens/wishlist.dart';
@@ -31,32 +32,14 @@ class MatchDetails extends StatefulWidget {
 class _MatchDetailsState extends State<MatchDetails> {
 
 
-  List <Match> _matches =[
-    Match("match1", "Anorthosis VS Omonoia", "Championship Game", 10.0
-        , "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Anorthosis_FC_logo.svg/1200px-Anorthosis_FC_logo.svg.png",
-        "Cup", 7000, true, true, "Anorthosis", "Omonoia", "29 October 2021"),
-
-    Match("match2", "AEL VS Aris", "Championship Game", 10.0
-        , "https://seeklogo.com/images/A/AEL-logo-4FD3D4029E-seeklogo.com.png",
-        "Championship", 4000, true, true, "AEL", "Aris", "30 October 2021"),
-
-    Match("match3", "Doxa VS Apoel", "Championship Game", 10.0
-        , "https://seeklogo.com/images/D/doxa-katokopias-fc-logo-4995917919-seeklogo.com.png",
-        "Championship", 4000, true, true, "Doxa", "Apoel", "30 October 2021"),
-
-
-    Match("match4", "AEK VS Ethnikos", "Championship Game", 10.0
-        , "https://seeklogo.com/images/A/aek-larnaca-logo-A2A11BB75C-seeklogo.com.png",
-        "Championship", 4000, true, true, "Aek", "Ethnikos", "31 October 2021"),
-
-    Match("match5", "Olympiakos VS Apollon", "Championship Game", 10.0
-        , "https://seeklogo.com/images/O/olympiakos-nicosia-logo-BE09F64A71-seeklogo.com.gif",
-        "Championship", 4000, true, true, "Olympiakos", "Apollon", "31 October 2021"),
-
-  ];
 
   @override
   Widget build(BuildContext context) {
+
+    // Provider for match list
+    final matchesProvider = Provider.of<Matches>(context);
+    List<Match> matchesList = matchesProvider.matches;
+
     return Scaffold(
       body:
       Stack(
@@ -180,15 +163,15 @@ class _MatchDetailsState extends State<MatchDetails> {
                         width: double.infinity,
                         height: 302,
                         child: ListView.builder(
-                            itemCount: _matches.length,
+                            itemCount: matchesList.length,
                             scrollDirection: Axis.horizontal,
                             itemBuilder: (BuildContext ctx, int index){
                               return FeedProducts(
-                                  id: _matches[index].id,
-                                  description:_matches[index].description,
-                                  price: _matches[index].price,
-                                  quantity: _matches[index].quantity,
-                                  imageURL: _matches[index].imageURL,
+                                  id: matchesList[index].id,
+                                  description:matchesList[index].description,
+                                  price: matchesList[index].price,
+                                  quantity: matchesList[index].quantity,
+                                  imageURL: matchesList[index].imageURL,
                                   isFavorite: true
 
                               );
